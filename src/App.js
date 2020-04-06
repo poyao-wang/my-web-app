4acca6f6f42d18882266588a5b4c2f0eada403c4
BoyoMac <>
2020-04-06 12:21:12 +0900

    Adding react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"; //React bootstrap
 
 function App() {
   return (
