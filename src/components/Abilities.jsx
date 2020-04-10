3df594d635196d1d3f495c783519c2deae11208f
BoyoMac <>
2020-04-10 18:31:22 +0900

    Profile mod

---
+++ src/components/Abilities.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
+import "./Abilities.css";
+
+class Abilities extends Component {
+  state = {};
+  render() {
+    return <Container className="abi-div"></Container>;
+  }
+}
+
+export default Abilities;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,55 +7,57 @@ class Profile extends Component {
   render() {
     return (
       <Container fluid className="profile-div">
-        <Container>
-          <h2>Profile</h2>
-          <p class="lead">A Self-Taught Programmer.</p>
-          <hr className="profile-hr"></hr>
-          <Row className="justify-content-center">
-            <Col xs={12} sm={2}>
-              <h3 className="text-sm-left">About me</h3>
-              <p className="text-sm-left">
-                I am a mechanical engineer. Working in automobile industry. I
-                started stydying programming since 2019.
-              </p>
-            </Col>
-            <Col xs={12} sm={4}>
-              <Image
-                roundedCircle
-                className="profile-img"
-                src="assets/me2.jpeg"
-                alt="Poyao Wang"
-              />
-            </Col>
-            <Col xs={12} sm={2}>
-              <h3 className="text-sm-left">Details</h3>
-              <p className="text-sm-left">
-                <strong>Name:</strong>
-                <br />
-                Poyao Wang
-                <br />
-                <strong>Age:</strong>
-                <br />
-                32 years
-                <br />
-                <strong>Location:</strong>
-                <br />
-                Tokyo, Japan
-              </p>
-            </Col>
-          </Row>
-          <Button variant="dark" className="linkedin-btn">
-            <a
-              className="linkedin-link"
-              title="Follow PoyaoWang on LinkedIn"
-              href="https://www.linkedin.com/in/poyao-wang-33860058/"
-              target="_blank"
-            >
-              <Image className="linkedin-img" src="assets/LI-In-Bug_bg_w.png" />
-              <span className="linkedin-text">Follow</span>
-            </a>
-          </Button>
-        </Container>
+        <Row className="justify-content-center">
+          <Col xs={10} sm={8}>
+            <h2>Profile</h2>
+            <p class="lead">A Self-Taught Programmer.</p>
+            <hr className="profile-hr"></hr>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs={10} sm={8} md={2}>
+            <h3 className="text-md-left">About me</h3>
+            <p className="text-md-left">
+              I am a mechanical engineer. Working in automobile industry. I
+              started stydying programming since 2019.
+            </p>
+          </Col>
+          <Col xs={10} sm={8} md={4}>
+            <Image
+              roundedCircle
+              className="profile-img"
+              src="assets/me2.jpeg"
+              alt="Poyao Wang"
+            />
+          </Col>
+          <Col xs={10} sm={8} md={2}>
+            <h3 className="text-md-left">Details</h3>
+            <p className="text-md-left">
+              <strong>Name:</strong>
+              <br />
+              Poyao Wang
+              <br />
+              <strong>Age:</strong>
+              <br />
+              32 years
+              <br />
+              <strong>Location:</strong>
+              <br />
+              Tokyo, Japan
+            </p>
+          </Col>
+        </Row>
+        <Button variant="dark" className="linkedin-btn">
+          <a
+            className="linkedin-link"
+            title="Follow PoyaoWang on LinkedIn"
+            href="https://www.linkedin.com/in/poyao-wang-33860058/"
+            target="_blank"
+          >
+            <Image className="linkedin-img" src="assets/LI-In-Bug_bg_w.png" />
+            <span className="linkedin-text">Follow</span>
+          </a>
+        </Button>
       </Container>
     );
   }
