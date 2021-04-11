996c8358c2731cf2eb6af4267aa9bf2e3b08a13a
Poyao Wang <michael90110@gmail.com>
2021-04-11 19:55:23 +0900

    Clear App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"; //React bootstrap
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About";
-import News from "./components/News";
 import CustomNavbar from "./components/CustomNavbar";
 
 function App() {
@@ -14,8 +11,6 @@ function App() {
       <div>
         <CustomNavbar />
         <Route exact path="/" component={Home} />
-        {/* <Route path="/about" component={About} />
-        <Route path="/news" component={News} /> */}
       </div>
     </Router>
   );
