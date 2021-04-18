40cb658daf77f7c0118e9ecfbb675ddab45226fe
Poyao Wang <michael90110@gmail.com>
2021-04-18 17:07:57 +0900

    Refactor data to Home for About
    Change About to  stateless function

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,49 +1,48 @@
 import React, { Component } from "react";
 import "./About.css";
-class About extends Component {
-  state = {};
 
-  col(imagePath = null, tilteText = "colTitle", pText = "content") {
+function About({ data }) {
+  const colsData = data.cols;
+
+  const col = (imagePath = null, tilteText = "colTitle", pText = "content") => {
     let newText = pText.split("\n").map((item, i) => <p key={i}>{item}</p>);
     return (
       <div className="about-div-col d-flex flex-column justify-content-center align-items-center">
         <div className="about-div-img-wrapper d-flex justify-content-center align-items-center">
-          <img className="about-div-img" src={imagePath} alt="Poyao Wang" />
+          <img className="about-div-img" src={imagePath} alt={tilteText} />
         </div>
         <h3>{tilteText}</h3>
         <p>{pText}</p>
       </div>
     );
-  }
+  };
 
-  render() {
-    return (
-      <div fluid className="about-div main-divs" id="about-div">
-        <div className="justify-content-center">
-          <h2>About Me</h2>
-          <p className="h2-subtitle">A Self-taught Full-Stack Developer</p>
-          <hr />
-        </div>
-        <div className="d-flex flex-column flex-md-row justify-content-center">
-          {this.col(
-            "assets/about-div-img1.png",
-            "Front-end",
-            `ReactJS Web App / Mobile App\nHTML / CSS / JavaScript\nAdobe Ps / Ai / Xd`
-          )}
-          {this.col(
-            "assets/about-div-img2.png",
-            "Back-end",
-            `RESTful API development\nCloud sever deployment\nNodeJS, mongoDB`
-          )}
-          {this.col(
-            "assets/about-div-img3.png",
-            "Languages",
-            `English / Chinese / Japanese\nBusiness level language skills\nMulti-language UI`
-          )}
-        </div>
+  return (
+    <div fluid className="about-div main-divs" id="about-div">
+      <div className="justify-content-center">
+        <h2>{data.title}</h2>
+        <p className="h2-subtitle">{data.subtitle}</p>
+        <hr />
       </div>
-    );
-  }
+      <div className="d-flex flex-column flex-md-row justify-content-center">
+        {col(
+          colsData[0].imgSrc,
+          colsData[0].titleText,
+          colsData[0].contentText
+        )}
+        {col(
+          colsData[1].imgSrc,
+          colsData[1].titleText,
+          colsData[1].contentText
+        )}
+        {col(
+          colsData[2].imgSrc,
+          colsData[2].titleText,
+          colsData[2].contentText
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,29 @@ import About from "./About";
 import Skills from "./Skills";
 import CustomNavbar from "./CustomNavbar";
 
+const data = {
+  about: {
+    title: "About Me",
+    subtitle: "A Self-taught Full-Stack Developer",
+    cols: [
+      {
+        imgSrc: "assets/about-div-img1.png",
+        titleText: "Front-end",
+        contentText: `ReactJS Web App / Mobile App\nHTML / CSS / JavaScript\nAdobe Ps / Ai / Xd`,
+      },
+      {
+        imgSrc: "assets/about-div-img2.png",
+        titleText: "Back-end",
+        contentText: `RESTful API development\nCloud sever deployment\nNodeJS, mongoDB`,
+      },
+      {
+        imgSrc: "assets/about-div-img3.png",
+        titleText: "Languages",
+        contentText: `English / Chinese / Japanese\nBusiness level language skills\nMulti-language UI`,
+      },
+    ],
+  },
+};
 class Home extends Component {
   state = {};
   render() {
@@ -18,7 +41,7 @@ class Home extends Component {
             Designer / Front-end / Back-end Developer
           </p>
         </div>
-        <About />
+        <About data={data.about} />
         <Skills />
         <Projects />
         <Contact />
