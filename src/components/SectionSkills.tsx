2a3c9b9d418df2cf39df49d3f7a1fac02a09395a
poyaowang <michael90110@gmail.com>
2021-09-17 00:06:04 +0900

    Imp translation to SectionAbout

--- src/components/SectionSkills.tsx
+++ src/components/SectionSkills.tsx
@@ -1,35 +1,94 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import * as TypeMainData from '../mainData.model';
 import IconWithContainer from './IconWithContainer';
 
-const dummySkillIcon = (
-  <IconWithContainer
-    iconSrc={'assets/skills/right-1.png'}
-    type={'skill'}
-    text="HTML"
-  />
-);
-
 const SectionSkills: React.FC = () => {
+  const { t } = useTranslation();
+
+  const sectionSkills: TypeMainData.SectionSkills = t('sectionSkills', {
+    returnObjects: true,
+  });
+
   return (
     <section className="section-skills">
-      <h2>About Me</h2>
-      <p className="p-h2">A Self-taught Full-Stack Developer</p>
+      <h2>{sectionSkills.title}</h2>
+      <p className="p-h2">{sectionSkills.subtitle}</p>
       <div className="skill-items">
-        <div className="skill-items__col mobile-hide">{dummySkillIcon}</div>
-        <div className="skill-items__col">
-          {dummySkillIcon}
-          {dummySkillIcon}
+        <div className="skill-items__col mobile-hide">
+          {
+            <IconWithContainer
+              type={'skill'}
+              iconSrc={sectionSkills.skillItems.lefter.imgSrc}
+              text={sectionSkills.skillItems.lefter.title}
+            />
+          }
         </div>
         <div className="skill-items__col">
-          {dummySkillIcon}
-          {dummySkillIcon}
-          {dummySkillIcon}
+          {
+            <IconWithContainer
+              type={'skill'}
+              iconSrc={sectionSkills.skillItems.left1.imgSrc}
+              text={sectionSkills.skillItems.left1.title}
+            />
+          }
+          {
+            <IconWithContainer
+              type={'skill'}
+              iconSrc={sectionSkills.skillItems.left2.imgSrc}
+              text={sectionSkills.skillItems.left2.title}
+            />
+          }
         </div>
         <div className="skill-items__col">
-          {dummySkillIcon}
-          {dummySkillIcon}
+          {
+            <IconWithContainer
+              type={'skill'}
+              iconSrc={sectionSkills.skillItems.mid1.imgSrc}
+              text={sectionSkills.skillItems.mid1.title}
+            />
+          }
+          {
+            <IconWithContainer
+              type={'skill'}
+              iconSrc={sectionSkills.skillItems.mid2.imgSrc}
+              text={sectionSkills.skillItems.mid2.title}
+            />
+          }
+          {
+            <IconWithContainer
+              type={'skill'}
+              iconSrc={sectionSkills.skillItems.mid3.imgSrc}
+              text={sectionSkills.skillItems.mid3.title}
+            />
+          }
         </div>
-        <div className="skill-items__col mobile-hide">{dummySkillIcon}</div>
+        <div className="skill-items__col">
+          {
+            <IconWithContainer
+              type={'skill'}
+              iconSrc={sectionSkills.skillItems.right1.imgSrc}
+              text={sectionSkills.skillItems.right1.title}
+            />
+          }
+          {
+            <IconWithContainer
+              type={'skill'}
+              iconSrc={sectionSkills.skillItems.right2.imgSrc}
+              text={sectionSkills.skillItems.right2.title}
+            />
+          }
+        </div>
+        <div className="skill-items__col mobile-hide">
+          {
+            <IconWithContainer
+              type={'skill'}
+              iconSrc={sectionSkills.skillItems.righter.imgSrc}
+              text={sectionSkills.skillItems.righter.title}
+            />
+          }
+        </div>
       </div>
     </section>
   );
