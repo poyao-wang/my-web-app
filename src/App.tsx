660d31513aa223de864311ae17219874b5669470
poyaowang <michael90110@gmail.com>
2021-09-18 17:15:19 +0900

    Imp routing
    Refactor the main page to PageHome.tsx. Route "/" to PageHome in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React, { Suspense } from 'react';
-import NavBar from './components/NavBar';
-import SectionAbout from './components/SectionAbout';
-import SectionContact from './components/SectionContact';
-import SectionHero from './components/SectionHero';
-import SectionProjects from './components/SectionProjects';
-import SectionSkills from './components/SectionSkills';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import PageHome from './components/PageHome';
 
 const App: React.FC = () => {
   return (
     <Suspense fallback="loading">
       <div className="App">
-        <div id="main-container">
-          <NavBar />
-          <SectionHero />
-          <SectionAbout />
-          <SectionSkills />
-          <SectionProjects />
-          <SectionContact />
-        </div>
+        <Router>
+          <Route exact path="/" component={PageHome} />
+        </Router>
       </div>
     </Suspense>
   );
---
+++ src/components/PageHome.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavBar from './NavBar';
+import SectionHero from './SectionHero';
+import SectionAbout from './SectionAbout';
+import SectionSkills from './SectionSkills';
+import SectionProjects from './SectionProjects';
+import SectionContact from './SectionContact';
+
+const PageHome: React.FC = () => {
+  return (
+    <div id="main-container">
+      <NavBar />
+      <SectionHero />
+      <SectionAbout />
+      <SectionSkills />
+      <SectionProjects />
+      <SectionContact />
+    </div>
+  );
+};
+
+export default PageHome;
