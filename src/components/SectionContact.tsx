9b477437e94bfb78c594485960cf9a1feb9b1add
poyaowang <michael90110@gmail.com>
2022-01-01 16:55:57 +0900

    Implement contact section bg svg
    new file:   src/assets/svgs/bg-btm.svg
    - Add new svg file for bottom bg
    
    new file:   src/components/SvgBg.tsx
    - Add new component SvgBg
    - Import bg-btm.svg as SvgBg.Btm
    
    modified:   src/components/SectionContact.tsx
    - Implemented SvgBg.Btm, wrapped with div#contact-bg
    - Wrapped other contents under div.section-contact__contents
    
    modified:   src/index.css
    - Remove background styles settings
    - Set #contact-bg to absolute as background
    - Set z-index: 1 for other contents, so that it won't be blocked
      by #contact-bg

--- src/components/SectionContact.tsx
+++ src/components/SectionContact.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 
 import * as TypeMainData from "../mainData.model";
 import IconWithContainer from "./IconWithContainer";
+import SvgBg from "./SvgBg";
 
 const SectionContact: React.FC = () => {
   const { t } = useTranslation();
@@ -12,23 +13,24 @@ const SectionContact: React.FC = () => {
   });
 
   return (
-    <section
-      id="page-home__section-contact"
-      className="section-contact"
-      style={{ backgroundImage: "url(assets/bg-bottom.png)" }}
-    >
-      <h2>{sectionContact.title}</h2>
-      <p className="p-h2">{sectionContact.subtitle}</p>
-      <div className="contact-items">
-        {sectionContact.contactItems.map((item, index) => (
-          <a key={index} href={item.href}>
-            <IconWithContainer
-              iconSrc={item.iconSrc}
-              type={"contact"}
-              text={item.text}
-            />
-          </a>
-        ))}
+    <section id="page-home__section-contact" className="section-contact">
+      <div id="contact-bg">
+        <SvgBg.Btm />
+      </div>
+      <div className="section-contact__contents">
+        <h2>{sectionContact.title}</h2>
+        <p className="p-h2">{sectionContact.subtitle}</p>
+        <div className="contact-items">
+          {sectionContact.contactItems.map((item, index) => (
+            <a key={index} href={item.href}>
+              <IconWithContainer
+                iconSrc={item.iconSrc}
+                type={"contact"}
+                text={item.text}
+              />
+            </a>
+          ))}
+        </div>
       </div>
     </section>
   );
---
+++ src/components/SvgBg.tsx
@@ -0,0 +1,7 @@
+import { ReactComponent as Btm } from "../assets/svgs/bg-btm.svg";
+
+const SvgBg = {
+  Btm: () => <Btm className="icon" />,
+};
+
+export default SvgBg;
